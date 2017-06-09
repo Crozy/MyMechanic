88da3d620b654a295b8d3fcf8003a439fa5056d4
Crozy <contact@paul-leuck.ovh>
2017-06-09 15:38:11 +0200

    Mise en place du SMS

--- app/src/main/java/com/example/paul/mymechanic/Questionnaire3.java
+++ app/src/main/java/com/example/paul/mymechanic/Questionnaire3.java
@@ -1,6 +1,7 @@
 package com.example.paul.mymechanic;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class Questionnaire3 extends AppCompatActivity {
 
@@ -26,16 +28,50 @@ public class Questionnaire3 extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 
-        final Button loginButton = (Button) findViewById(R.id.button_Demande_Intervention);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+//        final Button loginButton = (Button) findViewById(R.id.button_Demande_Intervention);
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Questionnaire3.this, Contact_Garage.class);
+//                startActivity(intent);
+//            }
+//
+//        });
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Questionnaire3.this, Contact_Garage.class);
-                startActivity(intent);
-            }
 
-        });
+//        final Button loginButton = (Button) findViewById(R.id.button_Demande_Intervention);
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                final Spinner Probleme = (Spinner) findViewById(R.id.spinnerProbleme);
+//                SMS.AjoutProbleme(Probleme.getSelectedItem().toString());
+//            }
+//
+//        });
+//
+    }
+
+    public void EnvoyéDevisSMS(View View) {
+
+        final Spinner Probleme = (Spinner) findViewById(R.id.spinnerProbleme);
+        SMS.AjoutProbleme(Probleme.getSelectedItem().toString());
+
+        String ListeDesVoyan;
+
+        ListeDesVoyan = "Voyant allumé : ";
+
+        for(int i = 0; i < SMS.GetListeDesVoyant().size(); i++) {
+            ListeDesVoyan = ListeDesVoyan +" \n "+ SMS.GetListeDesVoyant().get(i);
+        }
+
+        Intent sms = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:0102030405")); {
+        sms.putExtra("sms_body", "Marque : "+SMS.GetMarque()+"\n Type Commercial : "+SMS.GetTypeCommercial()+"\n Type Moteur : "+SMS.GetTypeMoteurt()+"\n Année : "+SMS.GetAnnee()+"\n Carburant : "+SMS.GetCarburant()+"\n"+ListeDesVoyan+"\n Problème rencontré : "+SMS.GetProbleme());
+        startActivity(sms);
+    }
+
 }
 
 }
+
